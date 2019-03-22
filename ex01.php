0cd4b2f2747ad47952060dd00f554f2dc5f2211b
AurelienBarbier <aurelien@wildcodeschool.fr>
2019-03-22 17:13:54 +0100

    clean comments

--- ex01.php
+++ ex01.php
@@ -1,18 +1,21 @@
 <?php
 
 /**
-    Appeler la fonction "returnParameter",
-    Envoyer le nombre 5 en paramètre;
-
-    Tips:
-    Nous avons déclaré la fonction;
-    Vous avez juste à l'appeler;
-    Ne cherchez pas trop loin;
-    Vous pouvez appeler plusieurs fois la fonction pour tester;
-
-    Utilisez la fonction console.log
-    pour afficher la valeur de retour de la fonction;
-*/
+ *
+ * Appeler la fonction "returnParameter",
+ * Envoyer le nombre 5 en paramètre;
+ *
+ *
+ * Tips:
+ *
+ *    Nous avons déclaré la fonction;
+ *    Vous avez juste à l'appeler;
+ *    Ne cherchez pas trop loin;
+ *    Vous pouvez appeler plusieurs fois la fonction pour tester;
+ *
+ *    Utilisez la fonction console.log
+ *    pour afficher la valeur de retour de la fonction;
+ */
 
 /**
  *  Déclaration de la fonction "returnParameter",
