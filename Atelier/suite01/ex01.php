d9ef3a03c7ede2a4c31b0537baf31de609508b13
AdelineDb <adeline.dubosc@hotmail.fr>
2019-03-25 22:55:45 +0100

    initial commit

---
+++ Atelier/suite01/ex01.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *
+ * Appeler la fonction "returnParameter",
+ * Envoyer le nombre 5 en paramètre;
+ *
+ *
+ * Tips:
+ *
+ *    Nous avons déclaré la fonction;
+ *    Vous avez juste à l'appeler;
+ *    Ne cherchez pas trop loin;
+ *    Vous pouvez appeler plusieurs fois la fonction pour tester;
+ *
+ *    
+ */
+
+/**
+ *  Déclaration de la fonction "returnParameter",
+ *  prenant un paramètre "$number",
+ *  qui retourne ce paramètre "nb";
+*/
+
+function returnParameter(int $number):int {
+    return $number;
+}
+
+//  écrire votre code sous ce commentaire
+
